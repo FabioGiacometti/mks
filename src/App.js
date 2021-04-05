40a2addc2e3cf1754628d0006a5cfd26a69fd680
fabio.giacometti <fabiog.inbox@gmail.com>
2021-04-05 17:28:32 -0300

    dark mode added

--- src/App.js
+++ src/App.js
@@ -22,7 +22,6 @@ function App() {
   return (
     <div className="app__container">
       <Modal data={showModal} onClose={() => setShowModal(false)}>
-        {" "}
       </Modal>
       <Header />
 
--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,11 +1,25 @@
 import React from 'react'
 import './Header.scss'
 
+const handleDarkModeSwitch = () => {
+    const body = document.querySelector('body')
+    const toggle = document.querySelector('label')
+    body.classList.toggle('dark')
+}
+
 const Header = () => {
     return (
         <div className="header__container">
-            <h1 className="title">Social Media Dashboard</h1>
-            <h2 className="subtitle">Total Followers: 23,004</h2>
+            <div className="header__titles">
+                <h1 className="title">Social Media Dashboard</h1>
+                <h2 className="subtitle">Total Followers: 23,004</h2>
+            </div>
+            <div className="toggle_container">
+                <input type="checkbox" name="checkbox" id="checkbox"/>
+                <label htmlFor="checkbox" className="label" onClick={() => handleDarkModeSwitch()}>
+                    <div className="ball"></div>
+                </label>
+            </div>
         </div>
     )
 }
