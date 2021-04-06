20c2cd9a96b8310c21b086851f76c2d255845515
fabio.giacometti <fabiog.inbox@gmail.com>
2021-04-06 09:13:42 -0300

    added logo and fixed css issues

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import './Header.scss'
+import Logo from '../../assets/images/Logo.png'
 
 let isDarkModeEnabled = false
 const handleDarkModeSwitch = () => {
@@ -13,8 +14,11 @@ const Header = () => {
     return (
         <div className="header__container">
             <div className="header__titles">
-                <h1 className="title">Social Media Dashboard</h1>
-                <h2 className="subtitle">Total Followers: 23,004</h2>
+                <img src={Logo} alt="" className="logo"/>
+                <div className="header__titles-container">
+                    <h1 className="title">Social Media Dashboard</h1>
+                    <h2 className="subtitle">Total Followers: 23,004</h2>
+                </div>
             </div>
             <div className="toggle_container" title={"Enable/Disable Dark Mode"}>
                 <input type="checkbox" name="checkbox" id="checkbox"/>
